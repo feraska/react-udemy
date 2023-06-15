fae7c701cabbbb1a716f86a1f704c669efcadf0b
unknown <you@example.com>
2023-06-15 22:27:24 +0300

    assigment 1

---
+++ src/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+const ExpenseItem = ({data}) => {
+    return(
+        <div className="expense-item">
+            <ExpenseDate date={data.date} />
+      <div className='expense-item__description'>
+        <h2>{data.title}</h2>
+        <div className='expense-item__price'>${data.amount}</div>
+      </div>
+        </div>
+    )
+}
+export default ExpenseItem
